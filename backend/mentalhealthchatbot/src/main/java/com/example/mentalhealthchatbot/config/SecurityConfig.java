9d141b6dcfa820a5445f135ad201294e0f50ae6b
shravani12d <shravani12d@gmail.com>
2026-06-30 14:40:03 +0530

    Update SecurityConfig.java

--- backend/mentalhealthchatbot/src/main/java/com/example/mentalhealthchatbot/config/SecurityConfig.java
+++ backend/mentalhealthchatbot/src/main/java/com/example/mentalhealthchatbot/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.example.mentalhealthchatbot.config;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +17,9 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -40,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+         config.setAllowedOrigins(List.of(frontendUrl, "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
